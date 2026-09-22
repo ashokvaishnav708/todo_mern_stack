f435c149c5bf3489c60ac34eeffec77de34aef83
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-09-22 19:49:38 +0200

    fix: mongodb connection

--- backend/index.ts
+++ backend/index.ts
@@ -10,17 +10,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const mongodb = process.env.MONGODB_STR;
-
 app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
 
 const PORT = process.env.PORT || 5000;
+const MONGO_USER = process.env.MONGO_ROOT_USERNAME;
+const MONGO_PASS = process.env.MONGO_ROOT_PASSWORD;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+
+const MONGO_URI = `mongodb://localhost:${MONGO_PORT}/${MONGO_DB_NAME}`;
 
 mongoose
-  .connect(mongodb || "")
-  .then(() => console.log("Connected to mongodb"))
+  .connect(MONGO_URI || "", {
+    auth: {
+      username: MONGO_USER,
+      password: MONGO_PASS,
+    },
+    authSource: "admin",
+  })
+  .then(() => console.log(`Connected to ${MONGO_URI}`))
   .catch((err) => {
-    console.error("Error connecting mongodb: ", err);
+    console.error(`Error connecting ${MONGO_URI}: `, err);
   });
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    console.log(`Server started on localhost at port ${PORT}`);
+  }
+});
