ec45980cd370c654d9adc95cc40642ace1b41276
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-09-23 17:09:41 +0200

    adding MONGO_URI in env file

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGO_USER = process.env.MONGO_ROOT_USERNAME;
-const MONGO_PASS = process.env.MONGO_ROOT_PASSWORD;
-
 const MONGO_URI = process.env.MONGO_URI || "";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI, {
-      auth: { username: MONGO_USER, password: MONGO_PASS },
-      authSource: "admin",
-    });
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Error connecting database: ", error);
